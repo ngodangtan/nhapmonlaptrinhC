4f2efb22a554cecae6819c9ea64106e006fdeaf9
Tan Ngo Dang <ngodangtan1994@gmail.com>
2024-11-24 11:13:46 +0700

    update more

--- Buoi_07/string_demo.cpp
+++ Buoi_07/string_demo.cpp
@@ -3,13 +3,18 @@
 //
 
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main() {
   string str = "Hello World";
-  char s[100];
+  char s1[100];
   char s2[100] = {'T','h','u',' ','D','u','c','\0'};
   char s4[100] = "Thu Duc";
-  cout << s4 << endl;
+  for (int i = 0; i < strlen(s4); i++) {
+    if (s4[i] >= 'A' && s4[i] <= 'Z') {
+      cout << s4[i] << endl;
+    }
+  }
   return 0;
 }
