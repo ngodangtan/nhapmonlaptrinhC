548b28503ccfa67c5766b3f5c88279f303dcc90a
Tan Ngo Dang <ngodangtan1994@gmail.com>
2024-11-24 10:55:15 +0700

    Add start qua trinh bai string.

---
+++ Buoi_07/string_demo.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Felix Ngo on 24/11/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str = "Hello World";
+  char s[100];
+  char s2[100] = {'T','h','u',' ','D','u','c','\0'};
+  char s4[100] = "Thu Duc";
+  cout << s4 << endl;
+  return 0;
+}
